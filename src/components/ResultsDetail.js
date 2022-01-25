466280fdf28c77657fdd798ee3f18c6d6cfa733e
opikputra32 <opikputra32@gmail.com>
2022-01-25 23:45:34 +0700

    fixing scrollview and adding container styles margin

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -3,7 +3,7 @@ import { View, Image, Text, StyleSheet } from 'react-native';
 
 const ResultsDetail = ({ result }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image style={styles.imageStyle} source={{ uri: result.image_url }} />
       <Text style={styles.nameStyle}>{result.name}</Text>
       <Text>
@@ -14,10 +14,14 @@ const ResultsDetail = ({ result }) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15,
+  },
   imageStyle: {
     width: 250,
     height: 120,
     borderRadius: 4,
+    marginBottom: 5,
   },
   nameStyle: {
     fontWeight: 'bold',
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,10 +4,11 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.id}
         renderItem={({ item }) => {
@@ -24,6 +25,11 @@ const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
   },
 });
 
