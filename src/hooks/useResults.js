37c7849b959de5ba7ed580b189d1d30cbe95f08c
opikputra32 <opikputra32@gmail.com>
2022-01-25 22:44:11 +0700

    extracting custom hooks to searchscreen

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,4 +1,40 @@
 import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
 
-export default () => {};
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    console.log('Hi There');
+    try {
+      const responses = await yelp.get('/search', {
+        params: {
+          // Ini sama dengan /search?limit=50
+          limit: 50,
+          term: searchTerm,
+
+          /**
+           * params : location
+           * Required if either latitude or longitude is not provided. This string indicates the geographic area
+           * to be used when searching for businesses.
+           * Examples: "New York City", "NYC", "350 5th Ave, New York, NY 10118".
+           * Businesses returned in the response may not be strictly within the specified location.
+           * @returns string
+           */
+          location: 'san jose',
+        },
+      });
+      setResults(responses.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  // searchApi('pasta');
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,43 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-    console.log('Hi There');
-    try {
-      const responses = await yelp.get('/search', {
-        params: {
-          // Ini sama dengan /search?limit=50
-          limit: 50,
-          term: searchTerm,
-
-          /**
-           * params : location
-           * Required if either latitude or longitude is not provided. This string indicates the geographic area
-           * to be used when searching for businesses.
-           * Examples: "New York City", "NYC", "350 5th Ave, New York, NY 10118".
-           * Businesses returned in the response may not be strictly within the specified location.
-           * @returns string
-           */
-          location: 'san jose',
-        },
-      });
-      setResults(responses.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  // searchApi('pasta');
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
