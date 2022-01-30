77c4be7e3f54180ec4c3b1ec38a1e68e40d8b48f
opikputra32 <opikputra32@gmail.com>
2022-01-31 00:36:03 +0700

    getting the api with helper

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
-  console.log(id);
+  // console.log(id);
+  // console.log(result);
+  /**
+   * function helper
+   * @param {returning base on yelp api} id
+   * @returns response
+   */
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <View>
       <Text>Results show</Text>
